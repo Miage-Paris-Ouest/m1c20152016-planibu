43c56f87b50abbddd07a55f5be609abcf1b47f25
pascalorsini <pascal.orsini@outlook.com>
2016-02-03 09:26:47 +0100

    1er push

---
+++ app/src/main/java/com/example/leys/m1c20152016_planibu/SelectionDiscipline.java
@@ -0,0 +1,40 @@
+package com.example.leys.m1c20152016_planibu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class SelectionDiscipline extends AppCompatActivity {
+
+    ListView lvDisciplines;
+    ArrayAdapter<String> adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_discipline);
+
+
+        //InputStream inputStream = getResources().openRawResource(R.raw.disciplines);
+        //lireCSV csvFile = new lireCSV(inputStream);
+        //List scoreList = csvFile.read();
+
+
+
+
+        lvDisciplines = (ListView) findViewById(R.id.listViewDisciplines);
+
+        String [] tabDisciplines = {"Droit et Science Politique","Langues et Littérature",
+                "Sciences Économiques","Sciences Humaines", "Sciences Sociales", "La BUlle"};
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tabDisciplines);
+
+        lvDisciplines.setAdapter(adapter);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/leys/m1c20152016_planibu/SelectionSousDiscipline.java
@@ -0,0 +1,13 @@
+package com.example.leys.m1c20152016_planibu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectionSousDiscipline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_sous_discipline);
+    }
+}
