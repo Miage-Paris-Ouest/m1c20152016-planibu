b1df394df83790d33ecb3a47865b0817eacb8264
pascalorsini <pascal.orsini@outlook.com>
2016-02-07 17:18:36 +0100

    lireCSV

---
+++ app/src/main/java/com/example/leys/m1c20152016_planibu/lireCSV.java
@@ -0,0 +1,46 @@
+package com.example.leys.m1c20152016_planibu;
+
+/**
+ * Created by Pascal on 07/02/2016.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class lireCSV {
+    InputStream inputStream;
+
+    public lireCSV(InputStream inputStream){
+        this.inputStream = inputStream;
+    }
+
+    public List read(){
+        List resultList = new ArrayList();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String csvLine;
+            while ((csvLine = reader.readLine()) != null) {
+                String[] row = csvLine.split(";");
+                resultList.add(Arrays.toString(row));
+                System.out.println(Arrays.toString(row));
+
+            }
+        }
+        catch (IOException ex) {
+            throw new RuntimeException("Error in reading CSV file: "+ex);
+        }
+        finally {
+            try {
+                inputStream.close();
+            }
+            catch (IOException e) {
+                throw new RuntimeException("Error while closing input stream: "+e);
+            }
+        }
+        return resultList;
+    }
+}
