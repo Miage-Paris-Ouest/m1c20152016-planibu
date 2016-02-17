dd8a81ea9d6f59bbef2f92024d6873fbb8c7ad74
Khedidja EL M'RABET <khedidja.elmrabet@gmail.com>
2016-02-17 17:55:17 +0100

    recherche

--- app/src/main/java/com/example/leys/m1c20152016_planibu/MainActivity.java
+++ app/src/main/java/com/example/leys/m1c20152016_planibu/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -22,7 +23,6 @@ import android.widget.Toast;
 import android.app.SearchManager;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
-
 import com.example.leys.m1c20152016_planibu.menu_principal_choix.SelectionContact;
 import com.example.leys.m1c20152016_planibu.menu_principal_choix.SelectionHoraires;
 import com.example.leys.m1c20152016_planibu.menu_principal_choix.SelectionInfoRessources;
@@ -41,6 +41,11 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if(toolbar != null) {
+
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -55,37 +60,43 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
+        MenuItem searchItem= menu.findItem(R.id.action_websearch);
 
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_websearch)
-                .getActionView();
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
 
         if (searchView != null) {
-            searchView.setSearchableInfo(searchManager .getSearchableInfo(getComponentName()));
-
+            SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
             searchView.setIconifiedByDefault(true);
+
             searchView.setOnQueryTextListener(new OnQueryTextListener(){
                     //this method will call while press (click) search button.
 
-                @Override
-                public boolean onQueryTextChange(String newText) {
-                    return false;
-                }
+
                     @Override
                     public boolean onQueryTextSubmit(String query) {
                         Intent intent = new Intent(MainActivity.this, resultatCote.class);
                         Bundle extras = new Bundle();
 
                         recherche = query;
+                        if (searchView != null) {
+                            searchView.setVisibility(View.INVISIBLE);
+                            searchView.setVisibility(View.VISIBLE);
+
+                        }
 
                         extras.putString("recherche", recherche);
                         intent.putExtras(extras);
+
                         startActivity(intent);
 
 
                         return true;
                     }
-
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
 
             });
         }
@@ -120,7 +131,7 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(MainActivity.this, SelectionParSousDiscipline.class);
             startActivity(intent);
 
-        } else {
+    }else {
             if (id == R.id.selectCote) {
                 Intent intent = new Intent(MainActivity.this, CsvResultActivity.class);
                 startActivity(intent);
