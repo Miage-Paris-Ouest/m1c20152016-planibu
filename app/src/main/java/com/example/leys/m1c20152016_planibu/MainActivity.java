a2079ea99a03ce2c53baf9f59c05310ff2a70ecf
Khedidja EL M'RABET <khedidja.elmrabet@gmail.com>
2016-02-24 13:04:31 +0100

    selection service
    choix de service par un clique sur un button qui affiche une image de
    service sur son emplacement dans le plan

--- app/src/main/java/com/example/leys/m1c20152016_planibu/MainActivity.java
+++ app/src/main/java/com/example/leys/m1c20152016_planibu/MainActivity.java
@@ -15,7 +15,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Switch;
@@ -40,12 +42,16 @@ public class MainActivity extends AppCompatActivity
 
     String recherche;
     NavigationView navigationView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final ImageView imgView1 = (ImageView) findViewById(R.id.imgview1);
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -56,29 +62,147 @@ public class MainActivity extends AppCompatActivity
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        selectService1();
+        selectService2();
+        selectService3();
+        selectService4();
+        selectService5();
+        selectService6();
+        selectService7();
+
+
 
         //MON SWITCH BOUTON POUR ACTIVER/DESACTIVER L AFFICHAGE DES SERVICES
         Switch mySwitch = (Switch) findViewById(R.id.main_switch_service);
         final ImageView planServicesOrNot = (ImageView)findViewById(R.id.main_planServiceOrNot);
         //set the switch to OFF
         mySwitch.setChecked(false);
+
         mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
+                    imgView1.setImageResource(android.R.color.transparent);
                     Toast.makeText(getApplicationContext(), "Services ON", Toast.LENGTH_SHORT).show();
                     planServicesOrNot.setImageResource(R.drawable.buplanvif);
+
+
                 } else {
                     Toast.makeText(getApplicationContext(), "Services OFF", Toast.LENGTH_SHORT).show();
                     planServicesOrNot.setImageResource(R.drawable.plan_sans_services_vif);
+
+
                 }
             }
         });
         //FIN DE MON SWITCH
+
+
+    }
+    public void selectService1() {
+
+        ImageButton doc_logo = (ImageButton) findViewById(R.id.imgbutton_docmini);
+
+        doc_logo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                final ImageView imgView1 = (ImageView) findViewById(R.id.imgview1);
+                imgView1.setImageResource(R.drawable.plan_sans_services_atelierdoc);
+
+            }
+        });
     }
 
+    public void selectService2() {
 
+        ImageButton grp_logo = (ImageButton) findViewById(R.id.imgbutton_grpmini);
+
+        grp_logo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                final ImageView imgView1 = (ImageView) findViewById(R.id.imgview1);
+                imgView1.setImageResource(R.drawable.plan_sans_services_groupe);
+
+            }
+        });
+    }
+    public void selectService3() {
+
+        ImageButton hand_logo = (ImageButton) findViewById(R.id.imgbutton_handmini);
+
+        hand_logo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                final ImageView imgView1 = (ImageView) findViewById(R.id.imgview1);
+                imgView1.setImageResource(R.drawable.plan_sans_services_handi);
+
+            }
+        });
+
+    }
+    public void selectService4() {
+
+        ImageButton imp_logo = (ImageButton) findViewById(R.id.imgbutton_imp_mini);
+
+
+        imp_logo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                final ImageView imgView1 = (ImageView) findViewById(R.id.imgview1);
+                imgView1.setImageResource(R.drawable.plan_sans_services_imp);
+
+            }
+        });
+    }
+    public void selectService5() {
+
+
+        ImageButton pret_logo = (ImageButton) findViewById(R.id.imgbutton_pretbupretetu);
+
+
+        pret_logo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                final ImageView imgView1 = (ImageView) findViewById(R.id.imgview1);
+                imgView1.setImageResource(R.drawable.plan_sans_services_peepeb);
+
+            }
+        });
+    }
+    public void selectService6() {
+
+        ImageButton pretdom_logo = (ImageButton) findViewById(R.id.imgbutton_pretdom);
+
+        pretdom_logo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                final ImageView imgView1 = (ImageView) findViewById(R.id.imgview1);
+                imgView1.setImageResource(R.drawable.plan_sans_services_pret_domicile);
+
+            }
+        });
+    }
+    public void selectService7() {
+
+        ImageButton repro_logo = (ImageButton) findViewById(R.id.imgbutton_repro);
+
+        repro_logo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                final ImageView imgView1 = (ImageView) findViewById(R.id.imgview1);
+                imgView1.setImageResource(R.drawable.plan_sans_services_presse);
+
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -89,7 +213,7 @@ public class MainActivity extends AppCompatActivity
         SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setIconifiedByDefault(false);
-        searchView.setQueryHint("Recherche par côte");
+        searchView.setQueryHint("Recherche par cÃ´te");
 
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -170,7 +294,7 @@ public class MainActivity extends AppCompatActivity
         }
 
         else if (id == R.id.droit) {
-              Intent intent = new Intent(MainActivity.this, SelectionSalleDroit.class);
+            Intent intent = new Intent(MainActivity.this, SelectionSalleDroit.class);
             startActivity(intent);
         }
         else if (id == R.id.seco) {
@@ -183,7 +307,7 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
         }
 
-     else if (id == R.id.bulle) {
+        else if (id == R.id.bulle) {
             Intent intent = new Intent(MainActivity.this, SelectionSalleBulle.class);
             startActivity(intent);
         }
