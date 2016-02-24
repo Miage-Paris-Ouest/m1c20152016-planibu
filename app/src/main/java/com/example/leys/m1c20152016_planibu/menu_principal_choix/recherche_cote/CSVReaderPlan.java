1f7c91ab38745dfa08306c07f7dd5cb27c4ddef2
pascalorsini <pascal.orsini@outlook.com>
2016-02-24 12:49:43 +0100

    comité

---
+++ app/src/main/java/com/example/leys/m1c20152016_planibu/menu_principal_choix/recherche_cote/CSVReaderPlan.java
@@ -0,0 +1,43 @@
+package com.example.leys.m1c20152016_planibu.menu_principal_choix.recherche_cote;
+
+/**
+ * Created by Pascal on 24/02/2016.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReaderPlan {
+
+    InputStream inputStream;
+    public CSVReaderPlan(InputStream is) {
+        this.inputStream = is;
+    }
+
+    public List<String[]> read() throws RuntimeException {
+        List<String[]> resultList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        try {
+            String csvLine;
+            while((csvLine = reader.readLine()) != null) {
+                String[] row = csvLine.split(";");
+                resultList.add(row);
+            }
+        } catch(IOException ex) {
+            throw new RuntimeException("Error in reading CSV file:" + ex);
+        } finally {
+            try{
+                inputStream.close();
+            } catch(IOException e) {
+                throw new RuntimeException("Error while closing input stream: " + e);
+            }
+        }
+
+        // test
+        return resultList;
+    }
+}
--- app/src/main/java/com/example/leys/m1c20152016_planibu/menu_principal_choix/recherche_cote/resultatCote.java
+++ app/src/main/java/com/example/leys/m1c20152016_planibu/menu_principal_choix/recherche_cote/resultatCote.java
@@ -1,8 +1,13 @@
 package com.example.leys.m1c20152016_planibu.menu_principal_choix.recherche_cote;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,19 +26,26 @@ public class resultatCote extends AppCompatActivity {
     String autre;
     String st;
     private ListView listView;
-    private ItemArrayAdapter2 itemArrayAdapter;
-    ArrayList<String[]> listeTest;
+    private ItemArrayAdapter itemArrayAdapter;
+    List<String> listeEtagere = new ArrayList<String>();
 
+    int Xleft;
+    int Xright;
+    int Ybottom;
+    int Yup;
+    String rowStr = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultat_cote);
 
-        String tv = "Résultat pour la cote ";
 
 
         Bundle extras = getIntent().getExtras();
         recherche = extras.getString("recherche");
+        String label = "Résultat pour la cote " + recherche;
+
+        setTitle(label);
 
 
         listView = (ListView) findViewById(R.id.listViewCSV);
@@ -41,7 +53,7 @@ public class resultatCote extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.csv_title_liste);
 
 
-        itemArrayAdapter = new ItemArrayAdapter2(getApplicationContext(), R.layout.single_list_item2);
+        itemArrayAdapter = new ItemArrayAdapter(getApplicationContext(), R.layout.single_list_item);
 
         Parcelable state = listView.onSaveInstanceState();
         listView.setAdapter(itemArrayAdapter);
@@ -51,12 +63,9 @@ public class resultatCote extends AppCompatActivity {
         CSVReader csv = new CSVReader(inputStream);
         List<String[]> scoreList = csv.read();
 
-        /* % for (String[] scoreData : scoreList) {
-            if (Arrays.asList(scoreData).contains(recherche)) {
 
-                itemArrayAdapter.add(scoreData);
-            }
-        }*/
+        String [] libelle = {"Salle", "Etagere", "Secteur/Discipline", "Sous-Discipline", "Cote"};
+        itemArrayAdapter.add(libelle);
 
         for (String[] scoreData : scoreList) {
             String str = Arrays.asList(scoreData).toString();
@@ -75,14 +84,97 @@ public class resultatCote extends AppCompatActivity {
                         itemArrayAdapter.add(scoreData);
 
                         System.out.println("scoreData : " + row[i]);
+                        System.out.println("scoreData2 : " + row[2]);
+                        System.out.println("scoreData3 : " + row[3]);
+                        System.out.println("scoreData4 : " + row[4]);
+                        System.out.println("scoreData0 : " + row[0]);
+                        System.out.println("scoreData1 : " + row[1]);
 
-                        String tvFinale = tv+ recherche;
-                        textView.setText(tvFinale);
+                        rowStr = row[1];
+                        listeEtagere.add(rowStr);
+
+                        // String tvFinale = tv+ recherche;
+                        //textView.setText(tvFinale);
                     }
 
                 }
             }
+
+
         }
+
+System.out.println("liste etagere : "+ listeEtagere.toString());
+
+String le = listeEtagere.toString().replace(" ","").replace("[","").replace("]","");
+        System.out.println("leeee : "+ le);
+
+        InputStream inputStream2 = getResources().openRawResource(R.raw.etageres_sh_position);
+        CSVReaderPlan csv2 = new CSVReaderPlan(inputStream2);
+        List<String[]> scoreList2 = csv2.read();
+
+        for (String[] scoreData : scoreList2) {
+            String str = Arrays.asList(scoreData).toString();
+            System.out.println("strX : " + str);
+
+            for (int i = 0; i<=1; i++) {
+                String[] row = str.split(",");
+                System.out.println("startswithY : " + row[i]);
+                if (row[i].startsWith("0", 0) ) {
+
+                    row[i]=row[i].replace("0","");
+                    System.out.println("startswithZ : " + row[i]);
+
+
+                }
+                    if (row[i].startsWith(le, 0) ) {
+
+                        System.out.println("startswithX : " + row[i]);
+
+                        itemArrayAdapter.add(scoreData);
+
+                        System.out.println("scoreDataX : " + row[i]);
+
+                        rowStr = row[1];
+                        listeEtagere.add(rowStr);
+
+                        // String tvFinale = tv+ recherche;
+                        //textView.setText(tvFinale);
+                    }
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+        ImageView iv =  (ImageView) findViewById(R.id.imageView3);
+
+        Bitmap bitMap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);  //creates bmp
+        bitMap = bitMap.copy(bitMap.getConfig(), true);     //lets bmp to be mutable
+
+        Canvas canvas = new Canvas(bitMap);                 //draw a canvas in defined bmp
+
+        Paint paint = new Paint();                          //define paint and paint color
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        //paint.setStrokeWidth(0.5f);
+        paint.setAntiAlias(true);                           //smooth edges
+
+
+        iv.setImageBitmap(bitMap);
+        //changed set image resource to set image background resource
+        iv.setBackgroundResource(R.drawable.plansh2);
+        //canvas.drawCircle(30, 40, 3, paint);
+        canvas.drawRect(Xleft,Yup,Xright,Ybottom,paint);
+        //invalidate to update bitmap in imageview
+        iv.invalidate();
 
 
         System.out.println("scoreList : " + scoreList);
